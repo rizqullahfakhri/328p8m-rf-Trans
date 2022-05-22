a51d7149325e3d25c5654a4779b4da3dc4975ea9
rizqullahfakhri <rizqullahfakhri23@gmail.com>
2022-05-23 02:58:57 +0700

    Add BER

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #define ADC_RESOLUTION 1024.0
 
 int val = 0;
+int x = 0;
 int counter = 0;
 int ledState = LOW;
 unsigned long previousMillis = 0;
@@ -50,29 +51,69 @@ void sendByte(byte input){
 
 void BER_Send(){
   //BER calculation
-  byte x[128] = {21,45,42,35,55,96,41,12,32,43
-                ,23,65,43,12,65,12,32,76,43,65
-                ,85,37,42,32,54,34,23,65,47,87
-                ,69,12,35,4,39,10,90,98,75,42
-                ,56,74,35,46,76,58,34,132,129,221
-                ,232,54,121,153,178,165,186,190,165,30
-                ,122,231,176,198,185,89,63,95,129,142
-                ,176,186,184,98,90,76,72,28,30,10
-                ,14,29,74,80,94,187,190,211,201,207
-                ,19,28,25,15,54,86,219,200,95,83
-                ,18,52,34,57,87,90,92,86,43,24
-                ,121,100,82,94,87,92,63,71,80,89
-                ,12,32,43,67,87,90,72,74
-                };
-  sendByte(x[val]);
-  sendByte('}');
+  // byte x[128] = {21,45,42,35,55,96,41,12,32,43
+  //               ,23,65,43,12,65,12,32,76,43,65
+  //               ,85,37,42,32,54,34,23,65,47,87
+  //               ,69,12,35,4,39,10,90,98,75,42
+  //               ,56,74,35,46,76,58,34,132,129,221
+  //               ,232,54,121,153,178,165,186,190,165,30
+  //               ,122,231,176,198,185,89,63,95,129,142
+  //               ,176,186,184,98,90,76,72,28,30,10
+  //               ,14,29,74,80,94,187,190,211,201,207
+  //               ,19,28,25,15,54,86,219,200,95,83
+  //               ,18,52,34,57,87,90,92,86,43,24
+  //               ,121,100,82,94,87,92,63,71,80,89
+  //               ,12,32,43,67,87,90,72,74
+  //               };
+  // sendByte(x[val]);
+  // sendByte('}');
+  // digitalWrite(LED_tx,HIGH);
+  // delay(100);
+  // digitalWrite(LED_tx,LOW);
+  // delay(100);
+  // Serial.println(x[val]);
+  // (val>=127)?val=0:val++;
+   int i;
+  
+  for(i=0; i<20; i++){
+  digitalWrite(8, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+}
+
+  digitalWrite(8, HIGH);
+  delayMicroseconds(3000);
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  
+    
+  for(i=0; i<128; i++){
+  if(x == 1)
+  digitalWrite(8, HIGH);
+  else
+  digitalWrite(8, LOW);
+  delayMicroseconds(500);
+  
+  if(x==1){
+    digitalWrite(8, LOW);
+    x = 0;
+  }
+  else{
+    digitalWrite(8, HIGH);
+    x = 1;
+  }
+  
+  delayMicroseconds(500);
+}//i
+
+  
+  digitalWrite(8, LOW);
+  // Serial.println("SENT");
   digitalWrite(LED_tx,HIGH);
   delay(100);
   digitalWrite(LED_tx,LOW);
-  delay(100);
-  Serial.println(x[val]);
-  (val>=127)?val=0:val++;
-  Serial.println("SENT");
+
 }
 
 float calculate_temp(){
