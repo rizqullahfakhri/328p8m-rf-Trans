959835dcb0cdf162133580d44840081d6f588511
rizqullahfakhri <rizqullahfakhri23@gmail.com>
2022-05-31 21:04:59 +0700

    upgrade data rate 2000 bps

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #define LED_tx 6
 #define pin_mode 3
 #define pin_stop 4
-#define TEMP_VAR 1100.0
-#define ADC_RESOLUTION 1024.0
+// #define TEMP_VAR 1100.0
+// #define ADC_RESOLUTION 1024.0
 
 int val = 0;
 int x = 0;
@@ -16,32 +16,32 @@ unsigned long previousMillis = 0;
 
 void sendByte(uint16_t input){
   int i;
-  
+  //Start Bit
   for(i=0; i<20; i++){
     digitalWrite(8, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(250);
     digitalWrite(8, LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(250);
   }
-
+  //Confirm Bit
   digitalWrite(8, HIGH);
-  delayMicroseconds(3000);
+  delayMicroseconds(1500);
   digitalWrite(8, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(250);
   
-    
+  //Message Bit 
   for(i=0; i<9; i++){
     if(bitRead(input,i)==1)
       digitalWrite(8, HIGH);
     else
       digitalWrite(8, LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(250);
   
     if(bitRead(input,i)==1)
       digitalWrite(8, LOW);
     else
       digitalWrite(8, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(250);
   }//i
 
   
@@ -49,50 +49,27 @@ void sendByte(uint16_t input){
 }
 
 void BER_Send(){
-  //BER calculation
-  // byte x[128] = {21,45,42,35,55,96,41,12,32,43
-  //               ,23,65,43,12,65,12,32,76,43,65
-  //               ,85,37,42,32,54,34,23,65,47,87
-  //               ,69,12,35,4,39,10,90,98,75,42
-  //               ,56,74,35,46,76,58,34,132,129,221
-  //               ,232,54,121,153,178,165,186,190,165,30
-  //               ,122,231,176,198,185,89,63,95,129,142
-  //               ,176,186,184,98,90,76,72,28,30,10
-  //               ,14,29,74,80,94,187,190,211,201,207
-  //               ,19,28,25,15,54,86,219,200,95,83
-  //               ,18,52,34,57,87,90,92,86,43,24
-  //               ,121,100,82,94,87,92,63,71,80,89
-  //               ,12,32,43,67,87,90,72,74
-  //               };
-  // sendByte(x[val]);
-  // sendByte('}');
-  // digitalWrite(LED_tx,HIGH);
-  // delay(100);
-  // digitalWrite(LED_tx,LOW);
-  // delay(100);
-  // Serial.println(x[val]);
-  // (val>=127)?val=0:val++;
    int i;
-  
+  //Start Bit
   for(i=0; i<20; i++){
   digitalWrite(8, HIGH);
-  delayMicroseconds(500);
+  delayMicroseconds(250);
   digitalWrite(8, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(250);
 }
-
+  //Confirm Bit
   digitalWrite(8, HIGH);
-  delayMicroseconds(3000);
+  delayMicroseconds(1500);
   digitalWrite(8, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(250);
   
-    
-  for(i=0; i<128; i++){
+  //Message Bit
+  for(i=0; i<1024; i++){
   if(x == 1)
   digitalWrite(8, HIGH);
   else
   digitalWrite(8, LOW);
-  delayMicroseconds(500);
+  delayMicroseconds(250);
   
   if(x==1){
     digitalWrite(8, LOW);
@@ -103,10 +80,9 @@ void BER_Send(){
     x = 1;
   }
   
-  delayMicroseconds(500);
+  delayMicroseconds(250);
 }//i
 
-  
   digitalWrite(8, LOW);
   // Serial.println("SENT");
   digitalWrite(LED_tx,HIGH);
@@ -118,7 +94,7 @@ void BER_Send(){
 
 float calculate_temp(){
   float value = analogRead(A0);
-  float tempC = 0.4071*value-64.306;
+  float tempC = 0.4071*value-81.814;
   return tempC;
 }
 
@@ -126,12 +102,14 @@ void SendTemp(){
   float C = calculate_temp();
   float finNum = C*10;
   int Num = finNum;
-  if(Num > 512){
-    Num = 511;
-  }
+  // if(Num > 512){
+  //   Num = 511;
+  // }
   digitalWrite(LED_tx,HIGH);
-  Serial.println(Num);
+  // Serial.println(Num);
   sendByte(Num);
+  digitalWrite(LED_tx,LOW);
+  delay(200);
   // int firstNum = C;
   // float decNum = abs(firstNum - C)*100;
   // int finDec = decNum;
@@ -141,8 +119,7 @@ void SendTemp(){
   // sendByte(finDec);
   // // Serial.println(finDec);
   // sendByte('}');
-  digitalWrite(LED_tx,LOW);
-  delay(200);
+  
 }
 
 void blinkLED(int interval){
